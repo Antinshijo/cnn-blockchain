369789a1cacbb41131e039d03d53bf4f13eff3b2
Antin Shijo <antinshijo@gmail.com>
2026-03-18 11:42:51 +0530

    what you changed

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ blockchain = Blockchain()
 # ================= HOME =================
 @app.route('/')
 def home():
-    return render_template("login.html")
+    return "WORKING ✅"
 
 
 # ================= DASHBOARD =================
